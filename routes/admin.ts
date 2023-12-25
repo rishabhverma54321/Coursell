aa3d023d1aed4f017efdd4a2cf9f7bf66110ac5f
rishabhverma54321 <rishabhverma54321@gmail.com>
2023-12-25 20:16:49 +0530

    fix: Adding validation to all routes input

--- routes/admin.ts
+++ routes/admin.ts
@@ -5,33 +5,58 @@ import { authenticationJwt, generateJwtToken } from "../middleware";
 
 const router = express.Router();
 
+const signupInput = z.object({
+  username: z.string().min(6).max(20),
+  password: z.string().min(4).max(20),
+});
+
+const loginInput = z.object({
+  username: z.string().min(6).max(20),
+  password: z.string().min(4).max(20),
+});
+
+const courseInput = z.object({
+  title: z.string().min(2),
+  description: z.string().min(12),
+  price: z.number().min(1),
+  imageLink: z.string().min(4),
+  published: z.boolean(),
+});
+
 // Admin routes
 router.post("/signup", async (req, res) => {
   // logic to sign up admin
-  const { username, password } = req.body;
-  if(typeof  username !== "string"){
-    res.status(411).json({
-      msg: "You sent wrong input, username should be string"
-    })
-    return;
+  const parsedsignupInput = signupInput.safeParse(req.body);
+  if (!parsedsignupInput?.success) {
+    return res.status(411).json({
+      msg: parsedsignupInput.error,
+    });
   }
+  const { username, password } = parsedsignupInput?.data;
   const existingAdmin = await Admin?.findOne({ username });
   if (existingAdmin) {
     res.status(403).json({ message: "Admin already exists" });
   } else {
     const newAdmin = new Admin({ username, password });
     await newAdmin.save();
-    const token = generateJwtToken(req.body);
+    const token = generateJwtToken(parsedsignupInput?.data);
     res.json({ message: "Admin created successfully", token });
   }
 });
 
 router.post("/login", async (req, res) => {
-  const { username, password } = req?.headers;
-  console.log("username",username)
-  const admin = await Admin?.findOne({ username }).maxTimeMS(30000);;
+  const parsedInput = loginInput.safeParse(req.headers);
+  if (!parsedInput?.success) {
+    res.status(411).json({
+      msg: parsedInput.error,
+    });
+    return;
+  }
+  const { username, password } = parsedInput?.data;
 
-  if (admin) {
+  const admin = await Admin?.findOne({ username }).maxTimeMS(30000);
+  const isAuthenticate = admin?.password === password;
+  if (isAuthenticate) {
     const token = generateJwtToken(admin);
     res.json({ message: "Logged in successfully", token });
   } else {
@@ -41,27 +66,40 @@ router.post("/login", async (req, res) => {
 
 router.post("/courses", authenticationJwt, async (req, res) => {
   // logic to create a course
-  const { title, description, price, imageLink, published } = req.body;
-  if (title && description && price && imageLink && published) {
-    const course = new Courses(req.body);
-    await course.save();
-    res.json({ message: "Course created successfully", courseId: course.id });
-  } else {
-    res.status(411).json({ message: "Please provide all values" });
+  const parsedInput = courseInput.safeParse(req.body);
+  if (!parsedInput?.success) {
+    res.status(411).json({
+      msg: parsedInput.error,
+    });
+    return;
   }
+  const course = new Courses(parsedInput?.data);
+  await course.save();
+  res.json({ message: "Course created successfully", courseId: course.id });
 });
 
-router.put("/courses/:courseId", authenticationJwt, async (req:any, res:any) => {
-  // logic to edit a course
-  const course = await Courses.findByIdAndUpdate(req.params.id, req.body, {
-    new: true,
-  });
-  if (course) {
-    res.json({ message: "Course updated successfully" });
-  } else {
-    res.status(403).json({ message: "Course ID not found" });
+router.put(
+  "/courses/:courseId",
+  authenticationJwt,
+  async (req: any, res: any) => {
+    const parsedInput = courseInput.safeParse(req.body);
+    if (!parsedInput?.success) {
+      res.status(411).json({
+        msg: parsedInput.error,
+      });
+      return;
+    }
+    // logic to edit a course
+    const course = await Courses.findByIdAndUpdate(req.params.id, parsedInput?.data, {
+      new: true,
+    });
+    if (course) {
+      res.json({ message: "Course updated successfully" });
+    } else {
+      res.status(403).json({ message: "Course ID not found" });
+    }
   }
-});
+);
 
 router.get("/courses", authenticationJwt, async (req, res) => {
   // logic to get all courses
--- routes/user.ts
+++ routes/user.ts
@@ -1,69 +1,96 @@
 import express from "express";
 import { User, Courses } from "../db";
-import { generateJwtToken,authenticationJwt } from "../middleware";
+import { z } from "zod";
+import { generateJwtToken, authenticationJwt } from "../middleware";
 
-const router = express.Router()
+const router = express.Router();
+
+const signupInput = z.object({
+  username: z.string().min(6).max(20),
+  password: z.string().min(4).max(20),
+});
+
+const loginInput = z.object({
+  username: z.string().min(6).max(20),
+  password: z.string().min(4).max(20),
+});
+
 
 router.post("/signup", async (req, res) => {
-    const {username, password} = req.body
-    const existingUser = await User.findOne({ username });
-    console.log("existinguser", username)
-    if (existingUser) { 
-      res.json({ message: "User already exists" });
-    } else {
-      const user = new User(req.body);
-      await user.save();
-      const token = generateJwtToken(user);
-  
-      res.json({ message: "User created successfully", token: token });
-    }
-    // logic to sign up user
-  });
-  
-  router.post("/login", async (req, res) => {
-    console.log("userLogin")
-    const { username } = req.body;
-    
-    const user = await User.findOne({ username });
-    if (user) {
-      const token = generateJwtToken(user);
-      res.json({ message: "Logged in successfully", token });
-    } else {
-      res.status(403).json({ message: "LoggIn failed" });
-    }
-    // logic to log in user
-  });
-  
-  router.get("/courses", authenticationJwt, async (req, res) => {
-    // logic to list all courses
-    const courses = await Courses.find({ published: true });
-    res.json({ courses });
-  });
-  
-  router.post("/courses/:courseId", authenticationJwt, async (req:any, res) => {
-    // logic to purchase a course
-    // const course_id = parseInt(req?.params?.courseId);
-    // const findCourse = COURSES?.find((course) => course?.courseId == course_id);
-    const course:any = await Courses.findById(req.params.courseId);
-    if (!course) {
-      res.status(403).json({ message: "Course not found" });
-    } else {
-      const user = await User.findOne({ username: req.user.username });
-      if (user) {
-        user.purchasedCourses.push(course);
-        await user.save();
-        res.json({ message: "Course purchased successfully" });
-      } else {
-        res.status(404).json({ message: "User not found" });
-      }
-    }
-  });
-  
-  router.get("/purchasedCourses", authenticationJwt, async (req:any, res) => {
-    // logic to view purchased courses
-    const user:any = await User.findOne({ username: req.user.username }).populate('purchasedCourses')
-    const courses = user.purchasedCourses;
-    res.json({ purchasedCourses: courses || [] });
-  });
+  const parsedsignupInput = signupInput.safeParse(req.body);
+  if (!parsedsignupInput?.success) {
+    return res.status(411).json({
+      msg: parsedsignupInput.error,
+    });
+  }
+  const { username, password } = parsedsignupInput?.data;
+  const existingUser = await User.findOne({ username });
 
-  export default router;
+  if (existingUser) {
+    res.json({ message: "User already exists" });
+  } else {
+    const user = new User(req.body);
+    await user.save();
+    const token = generateJwtToken(user);
+
+    res.json({ message: "User created successfully", token: token });
+  }
+  // logic to sign up user
+});
+
+router.post("/login", async (req, res) => {
+  const parsedInput = loginInput.safeParse(req.headers);
+  if (!parsedInput?.success) {
+    res.status(411).json({
+      msg: parsedInput.error,
+    });
+    return;
+  }
+  const { username, password } = parsedInput?.data;
+
+  const user = await User.findOne({ username });
+  const isAuthenticate = user?.password === password;
+  if (isAuthenticate) {
+    const token = generateJwtToken(user);
+    res.json({ message: "Logged in successfully", token });
+  } else {
+    res.status(403).json({ message: "LoggIn failed" });
+  }
+  // logic to log in user
+});
+
+router.get("/courses", authenticationJwt, async (req, res) => {
+  // logic to list all courses
+  const courses = await Courses.find({ published: true });
+  res.json({ courses });
+});
+
+router.post("/courses/:courseId", authenticationJwt, async (req: any, res) => {
+  // logic to purchase a course
+  // const course_id = parseInt(req?.params?.courseId);
+  // const findCourse = COURSES?.find((course) => course?.courseId == course_id);
+  const course: any = await Courses.findById(req.params.courseId);
+  if (!course) {
+    res.status(403).json({ message: "Course not found" });
+  } else {
+    const user = await User.findOne({ username: req.user.username });
+    if (user) {
+      user.purchasedCourses.push(course);
+      await user.save();
+      res.json({ message: "Course purchased successfully" });
+    } else {
+      res.status(404).json({ message: "User not found" });
+    }
+  }
+});
+
+router.get("/purchasedCourses", authenticationJwt, async (req: any, res) => {
+  // logic to view purchased courses
+  const user: any = await User.findOne({
+    username: req.user.username,
+  }).populate("purchasedCourses");
+  const courses = user.purchasedCourses;
+  res.json({ purchasedCourses: courses || [] });
+});
+
+export default router;
